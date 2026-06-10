8f326c89172b263aa439d079030e035160971707
CRIS-LI-DEV <cristianlillocespedes@gmail.com>
2026-06-10 17:36:54 -0400

    ACTUALIZACION

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import SensorViewSet, LecturaViewSet,CargaMasivaLecturasView
+from api.views import SensorViewSet, LecturaViewSet
 
 
 router = DefaultRouter()
@@ -11,8 +11,8 @@ router.register(r'lecturas', LecturaViewSet, basename='lectura')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Las rutas de la API quedan directo en la raíz o bajo el prefijo que elijas
+  
     path('api/', include(router.urls)),
-    path('api/lecturas/carga-masiva/', CargaMasivaLecturasView.as_view(), name='carga-masiva-lecturas'),
+  
 
 ]
