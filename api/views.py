2882a4ea368f8cae4dcaf2e310a8f42955738225
CRIS-LI-DEV <cristianlillocespedes@gmail.com>
2026-06-10 17:33:09 -0400

    ACTUALIZACION

--- api/views.py
+++ api/views.py
@@ -2,8 +2,7 @@ from rest_framework import viewsets
 from .models import Sensor, Lectura
 from .serializers import SensorSerializer, LecturaSerializer
 from django.db import transaction
-
-
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
