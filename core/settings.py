9f0a7561da799b8133711de12d704efeb549cf64
CRIS-LI-DEV <cristianlillocespedes@gmail.com>
2026-06-17 23:56:53 -0400

    actu

--- core/settings.py
+++ core/settings.py
@@ -25,11 +25,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles','corsheaders','api'
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'api',
 ]
 
 MIDDLEWARE = [
-     'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
