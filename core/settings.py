e0f19931c88ad9bb28c91b3e7a90fd8a88d43ef6
CRIS-LI-DEV <cristianlillocespedes@gmail.com>
2026-06-17 20:46:14 -0400

    ACTULIZAR

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = ["parcela-production.up.railway.app/sensores"]
+CORS_ALLOWED_ORIGINS = [
+    "parcela-production.up.railway.app",
+    ]
 
 ALLOWED_HOSTS = ['*', "parcela-production.up.railway.app/sensores"]
 
